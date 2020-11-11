2ae97ddfdb5f3fd227c701bdd579fd8c887f5dda
Grant Mccloskey <gmccloskey@google.com>
2020-11-10 20:32:22 -0800

    Update with pulling in config options

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CodeLinks
 VS Code Plugin to have configurable right click option for opening a site based on line number and code path.
+
+You can update the URI and Regex to Strip from *Settings* on the extention config.
--- codelink/src/extension.ts
+++ codelink/src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const strip: unknown = vscode.workspace.getConfiguration("codelink").get("stripRegex");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +10,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "codelink" is now active!');
+	let strip: unknown = vscode.workspace.getConfiguration("codelink").get("stripRegex");
+	let uri: unknown = vscode.workspace.getConfiguration("codelink").get("uriRegex");
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -21,7 +24,8 @@ export function activate(context: vscode.ExtensionContext) {
 		if (activeEditor) {
 			let line = activeEditor.selection.active.line;
 			let folderPath = activeEditor.document.fileName;
-			vscode.window.showInformationMessage("Hello World from codelink! " + line +":"+vscode.workspace.getWorkspaceFolder(activeEditor.document.uri)?.name + ":" + activeEditor.document.uri);
+			// vscode.window.showInformationMessage("Hello World from codelink! " + line +":"+vscode.workspace.getWorkspaceFolder(activeEditor.document.uri)?.name + ":" + activeEditor.document.uri);
+			vscode.window.showInformationMessage("Hello lala " + strip + " " + uri);
 			// vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('http://www.pinkbike.com/news/fail-of-the-month-june-2016.html'));
 		}
 	});
